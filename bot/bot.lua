d57d409339160ff1fd49e4a0f1e4508557c0dd87
vahidhoseini <vahidhoseini42@gmail.com>
2016-04-06 07:01:27 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -236,8 +236,8 @@ function create_config( )
   print ('saved config into ./data/config.lua')
 end
 
-function on_our_id (id)
-  our_id = id
+function on_our_id (197385822)
+  our_id =197385822
 end
 
 function on_user_update (user, what)
